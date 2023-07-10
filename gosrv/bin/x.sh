d3a56b7566f41ad7ca327f9b7777f2971006502c
ivan.huang <ivan.huang@openpie.com>
2023-07-10 14:12:03 +0800

    add bin test

---
+++ gosrv/bin/x.sh
@@ -0,0 +1,8 @@
+mkdir -p log
+pkill -9 proxy
+pkill -9 schedule 
+pkill -9 lake 
+pkill -9 consul
+
+cmd="configure tenant_mode=disabled; writemode on; begin; clearrange \"\" \\xFF;  commit;"
+fdbcli --exec "$cmd" || true
